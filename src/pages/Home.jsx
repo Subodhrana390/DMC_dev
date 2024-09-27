205404eab0fafc8e8480e8a75663e16feb71a35c
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-09-27 13:14:40 +0530

    remove  news and subscribe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,12 @@ import Loader from '../components/Loader'
 
 const HeroSection = lazy(() => import("../components/HeroSection"));
 const About = lazy(() => import("../components/About"));
-const JoinNow = lazy(() => import("../components/JoinNow"));
-const NewsSection = lazy(() => import("../components/NewsSection"));
 
 const Home = () => {
   return (
     <Suspense fallback={<Loader />}>
       <HeroSection />
       <About />
-      <JoinNow />
-      <NewsSection />
     </Suspense>
   );
 };
