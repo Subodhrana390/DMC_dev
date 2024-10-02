3aef99a5b33d2be4222a675973ee04b09a3e0eca
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-10-02 14:06:18 +0530

    flyer updated

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import img1 from '../assets/Events/IMG_4042.jpg'
-import img2 from '../assets/Events/Scanned_20240925-1846-26.jpg'
-import img3 from "../assets/Events/IMG_20240812_124541.jpg";
-
+import CanvaSep from "../assets/Events/canva_september.jpg";
+import RecuritAugust from "../assets/Events/recurit_august.jpg";
 const Highlights = () => {
   return (
     <section id="highlights">
@@ -11,7 +9,7 @@ const Highlights = () => {
         <div className="row g-4 justify-content-md-center">
           <div className="col-sm-12 col-md-6  col-lg-4">
             <img
-              src={img2}
+              src={CanvaSep}
               alt=""
               height={200}
               style={{
@@ -36,7 +34,7 @@ const Highlights = () => {
           </div>
           <div className="col-sm-12 col-md-6  col-lg-4">
             <img
-              src={img3}
+              src={RecuritAugust}
               alt=""
               height={200}
               style={{
@@ -57,31 +55,6 @@ const Highlights = () => {
               }}
             >
               New Recruitment 2024
-            </h4>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-4">
-            <img
-              src={img1}
-              alt=""
-              height={200}
-              style={{
-                objectFit: "cover",
-                borderRadius: "10px",
-                width: "100%",
-                boxShadow: "0 2px 0px 2px #bfd0d400",
-              }}
-            />
-            <h4
-              style={{
-                fontSize: "20px",
-                marginTop: "5px",
-                fontWeight: "900",
-                textAlign: "center",
-                userSelect: "none",
-                cursor: "pointer",
-              }}
-            >
-              Canva Events 2023
             </h4>
           </div>
         </div>
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import E1 from "../assets/Events/E1.jpg";
-import E2 from "../assets/Events/E2.jpg";
-import E3 from "../assets/Events/E3.jpg";
-import E4 from "../assets/Events/E4.jpg";
-import { FaClock } from "react-icons/fa";
+import BlogWritting from "../assets/Events/blog_writting.jpg";
+import CanvaMArch from "../assets/Events/canva_march.jpg";
+import Reel from "../assets/Events/Reel_video.jpg";
 import { useEffect } from "react";
 import Aos from "aos";
 
 const Events = () => {
-
   useEffect(() => {
     Aos.init({
       duration: 1000,
@@ -35,7 +32,7 @@ const Events = () => {
       <div className="event_List row g-4">
         <div className="Event_Card col-sm-3" data-aos="fade-left">
           <div className="Event_Card_img">
-            <img src={E1} alt="" loading="lazy" />
+            <img src={CanvaMArch} alt="" loading="lazy" />
           </div>
           <div className="Event_Card_content">
             <div className="event_title">Canva Workshop</div>
@@ -48,7 +45,7 @@ const Events = () => {
         </div>
         <div className="Event_Card col-sm-3" data-aos="fade-right">
           <div className="Event_Card_img">
-            <img src={E2} alt="" loading="lazy" />
+            <img src={Reel} alt="" loading="lazy" />
           </div>
           <div className="Event_Card_content">
             <div className="event_title">Reel Making Competition</div>
@@ -60,7 +57,7 @@ const Events = () => {
         </div>
         <div className="Event_Card col-sm-3" data-aos="fade-left">
           <div className="Event_Card_img">
-            <img src={E3} alt="" loading="lazy" />
+            <img src={BlogWritting} alt="" loading="lazy" />
           </div>
           <div className="Event_Card_content">
             <div className="event_title">blog writing competition</div>
@@ -68,19 +65,6 @@ const Events = () => {
               The Blog Writing Competition invites participants to showcase
               their writing skills by crafting compelling and insightful blog
               posts.
-            </div>
-          </div>
-        </div>
-        <div className="Event_Card col-sm-3" data-aos="fade-right">
-          <div className="Event_Card_img">
-            <img src={E4} alt="" loading="lazy" />
-          </div>
-          <div className="Event_Card_content">
-            <div className="event_title">Reels Competition</div>
-            <div className="event_description">
-              The Reels Competition encourages participants to create short,
-              engaging videos that highlight creativity, storytelling, and
-              content creation skills.
             </div>
           </div>
         </div>
