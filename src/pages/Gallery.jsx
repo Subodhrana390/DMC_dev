520417d926fb2125ea2b9ba07a730a7bab392654
Subodh Rana <subodhrana390@gmail.com>
2024-10-05 20:37:21 +0530

    Loader

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -40,7 +40,6 @@ const Gallery = () => {
     return (
       <div className="d-flex justify-content-center align-items-center vh-100">
         <div className="spinner-border" role="status">
-          <span className="sr-only">Loading...</span>
         </div>
       </div>
     ); // Bootstrap spinner for loading
