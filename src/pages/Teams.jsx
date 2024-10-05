a0eddd4a37955df7c04c10ecab722ab984c5e620
Subodh Rana <subodhrana390@gmail.com>
2024-10-05 20:41:49 +0530

    Team page added

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,17 +1,48 @@
 import React from "react";
-import cardImg from "../assets/core.png";
+
 const Teams = () => {
   return (
-    <div className="container text-center">
-      <h4>Teams</h4>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <a href="/members">Core Team</a>
+    <div className="container my-5">
+      <h2 className="text-center mb-5">Our Teams</h2>
+      
+      <div className="row">
+        {/* Core Team Card */}
+        <div className="col-md-6 mb-4">
+          <div className="card shadow-sm h-100">
+            <img
+              src="https://blogimage.vantagecircle.com/content/images/2020/08/teamwork-and-team-building.png" 
+              className="card-img-top" 
+              alt="Core Team"
+            />
+            <div className="card-body text-center">
+              <h5 className="card-title">Core Team</h5>
+              <p className="card-text">
+                The Core Team is the driving force behind our organization, ensuring success through leadership and teamwork.
+              </p>
+              <a href="/teams" className="btn btn-primary">
+                Meet the Core Team
+              </a>
+            </div>
           </div>
-          <div className="col-sm-12 col-md-6">
-            {" "}
-            <a href="/members">Sub Core Team</a>
+        </div>
+
+        {/* Sub Core Team Card */}
+        <div className="col-md-6 mb-4">
+          <div className="card shadow-sm h-100">
+            <img
+              src="https://blogimage.vantagecircle.com/content/images/2020/08/teamwork-and-team-building.png"
+              className="card-img-top" 
+              alt="Sub Core Team"
+            />
+            <div className="card-body text-center">
+              <h5 className="card-title">Sub Core Team</h5>
+              <p className="card-text">
+                The Sub Core Team supports the Core Team in executing strategies and achieving goals.
+              </p>
+              <a href="/teams" className="btn btn-secondary">
+                Meet the Sub Core Team
+              </a>
+            </div>
           </div>
         </div>
       </div>
